0e1320fa5ef9944021ba1d33a1343dd14f9b1808
Sushruti Nihale <sushrutin6@gmail.com>
2022-03-18 13:13:33 +0530

    added patch and delete requests for user

--- 11-ExpressFileUploads/src/index.js
+++ 11-ExpressFileUploads/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const userController = require("../controllers/users.controllers");
 
+app.use(express.json());
 app.use("/users",userController);
 
 module.exports = app;
