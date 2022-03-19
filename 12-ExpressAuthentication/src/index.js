8ec9b05031d9b65d009a5a658fcc57b1344ca63d
Sushruti Nihale <sushrutin6@gmail.com>
2022-03-19 16:09:20 +0530

    adding files

---
+++ 12-ExpressAuthentication/src/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+const userController = require("./controllers/users.controller");
+
+app.use(express.json());
+app.use("/users",userController);
+
+module.exports = app;
